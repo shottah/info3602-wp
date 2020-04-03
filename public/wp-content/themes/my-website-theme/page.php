1b0afe79ed8b79f205418f60eb2effb3194769e7
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-02 23:15:50 -0400

    Breadcrumb on child page only. Also dynamic.

--- public/wp-content/themes/my-website-theme/page.php
+++ public/wp-content/themes/my-website-theme/page.php
@@ -15,10 +15,14 @@ while (have_posts()) { the_post(); ?>
 
  <div class="container container--narrow page-section">
 
+<?php
+$PAGE_PARENT = wp_get_post_parent_id(get_the_id());
+if ($PAGE_PARENT) {
+ ?>
    <div class="metabox metabox--position-up metabox--with-home-link">
-     <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+     <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($PAGE_PARENT) ?></a> <span class="metabox__main"><?php echo the_title() ?></span></p>
    </div>
-
+<?php } ?>
    <div class="page-links">
      <h2 class="page-links__title"><a href="#">About Us</a></h2>
      <ul class="min-list">
