0e871e21fda99aa731a2924bc56747cd65d0c699
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-02 22:26:26 -0400

    Add javascript for homepage slider to function.

--- public/wp-content/themes/my-website-theme/functions.php
+++ public/wp-content/themes/my-website-theme/functions.php
@@ -13,6 +13,11 @@ function university_files() {
   wp_enqueue_style( 'university_main_styles', get_stylesheet_uri());
   wp_enqueue_style('font-awesome','https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('custom-google-font','https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+
+  // The following will activate javascript code that
+  // is required for various elements on the website
+  // such as the slider on the homepage.
+  wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0',true);
 }
 
 // This wordpress hook is performing an action on any
