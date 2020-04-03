56dd4d7d9958830e7681721031fb4c7e7671b853
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-02 23:56:29 -0400

    Make side-menu and breadcrumbs dynamic. Shows on only parent/child pages.

--- public/wp-content/themes/my-website-theme/page.php
+++ public/wp-content/themes/my-website-theme/page.php
@@ -16,34 +16,54 @@ while (have_posts()) { the_post(); ?>
  <div class="container container--narrow page-section">
 
 <?php
+// We should check to see if there is a valid parent
+// of this page, and also check to see whether there
+// are valid children of this page.
+//
+// A page without any parent or children does not need
+// breadcrumbs or side menu for internal navigation. The
+// control block below will not apply unless either of
+// the conditions are met.
 $PAGE_PARENT = wp_get_post_parent_id(get_the_id());
+$PAGE_HAS_CHILD = get_pages(array('child_of' => get_the_id()));
 if ($PAGE_PARENT) {
  ?>
+ <!-- BREADCRUMB CONTENT -->
    <div class="metabox metabox--position-up metabox--with-home-link">
-     <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($PAGE_PARENT) ?></a> <span class="metabox__main"><?php echo the_title() ?></span></p>
+     <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo ($PAGE_PARENT) ? get_the_title($PAGE_PARENT) : the_title() ?></a> <span class="metabox__main"><?php echo the_title() ?></span></p>
    </div>
-<?php } ?>
+   <!-- END BREADCRUMB -->
+<?php
+}
+if ($PAGE_PARENT or $PAGE_HAS_CHILD) {
+?>
+<!-- SIDEMENU NAV -->
    <div class="page-links">
-     <h2 class="page-links__title"><a href="#">About Us</a></h2>
+     <h2 class="page-links__title"><a href="#"><?php echo the_title() ?></a></h2>
      <ul class="min-list">
-       <li class="current_page_item"><a href="#">Our History</a></li>
-       <li><a href="#">Our Goals</a></li>
+       <?php wp_list_pages(array('title_li' => NULL, 'child_of' => ($PAGE_PARENT) ? $PAGE_PARENT : get_the_id())); ?>
      </ul>
    </div>
-
+   <!-- END SIDEMENU -->
+<?php
+}
+?>
    <div class="generic-content">
      <?php the_content() ?>
    </div>
 
  </div>
 
- <div class="page-section page-section--beige">
+<!-- GENERIC CONTENT 2 -->
+ <!-- <div class="page-section page-section--beige">
    <div class="container container--narrow generic-content">
      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
 
      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
    </div>
- </div>
+ </div> -->
+ <!-- END GENERIC CONTENT 2 -->
+
 <?php
 }
 ?>
