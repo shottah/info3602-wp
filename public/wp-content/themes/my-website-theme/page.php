29a6f17ca90e9c2d9538769adc9ffc5df9d87e44
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-02 22:52:43 -0400

    Replace header non-image with real image.

--- public/wp-content/themes/my-website-theme/page.php
+++ public/wp-content/themes/my-website-theme/page.php
@@ -4,7 +4,7 @@
 <?php
 while (have_posts()) { the_post(); ?>
  <div class="page-banner">
-   <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg);"></div>
+   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>);"></div>
    <div class="page-banner__content container container--narrow">
      <h1 class="page-banner__title">Our History</h1>
      <div class="page-banner__intro">
