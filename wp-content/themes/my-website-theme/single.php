e125413f21c6ec634a7f4d907ee9ed7959bc1047
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-05 14:30:52 -0400

    Add metabox area to template.

--- wp-content/themes/my-website-theme/single.php
+++ wp-content/themes/my-website-theme/single.php
@@ -17,7 +17,20 @@
     </div>
   </div>
 
+
+
   <div class="container container--narrow page-section">
+    <div class="metabox metabox--position-up metabox--with-home-link">
+      <p>
+        <a class="metabox__blog-home-link" href="<?php echo site_url( $path = '/blog') ?>">
+          <i class="fa fa-home" aria-hidden="true"></i> Blog Home
+        </a>
+        <span class="metabox__main">
+          Posted by <?php the_author_posts_link(  ); ?> on <?php the_time( $d = 'd M, Y' ) ?> in <?php echo get_the_category_list( $separator = ', ' ) ?>
+        </span>
+      </p>
+    </div>
+
     <div class="generic-content">
       <?php the_content(  ) ?>
     </div>
