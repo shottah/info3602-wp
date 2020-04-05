74ab0f6175aa808bc578868cbf6500a8bd52ae30
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-05 14:22:17 -0400

    Add ontent area to template.

--- wp-content/themes/my-website-theme/single.php
+++ wp-content/themes/my-website-theme/single.php
@@ -16,6 +16,12 @@
       </div>
     </div>
   </div>
+
+  <div class="container container--narrow page-section">
+    <div class="generic-content">
+      <?php the_content(  ) ?>
+    </div>
+  </div>
   <?php
   endwhile
 ?>
