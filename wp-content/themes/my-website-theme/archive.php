69dc30c613cf31058f92edb6719377a42abbad73
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-05 14:33:40 -0400

    Duplicate index.php -> archive.php

---
+++ wp-content/themes/my-website-theme/archive.php
@@ -0,0 +1,44 @@
+<?php
+
+get_header(  );
+
+?>
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">A Static One</h1>
+    <div class="page-banner__intro">
+      <p>Learn how the school of your dreams got started.</p>
+    </div>
+  </div>
+</div>
+<div class="container container--narrow page-section">
+<?php
+  while(have_posts()):
+    the_post(); ?>
+    <div class="post-item">
+      <h2 class="headline headline--medium headline--post-title">
+        <a href="<?php the_permalink( $post ) ?>">
+          <?php the_title( ) ?>
+        </a>
+      </h2>
+      <div class="metabox">
+        <p>Posted by <?php the_author( ) ?> on <?php the_time( $d = 'd M, Y' ) ?> in <?php echo get_the_category_list( $separator = ', ') ?></p>
+      </div>
+      <div class="generic-centent">
+        <p><?php the_excerpt() ?></p>
+      </div>
+      <p>
+        <a class="btn btn--blue" href="<?php the_permalink( $post ) ?>">Continue Reading &raquo;</a>
+      </p>
+    </div>
+    <?php
+  endwhile;
+  echo paginate_links(  );
+?>
+</div>
+<?php
+
+get_footer(  );
+
+ ?>
