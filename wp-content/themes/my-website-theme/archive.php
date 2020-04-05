6c7f2cad65af48ef221635d557f6237a3caa485b
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-05 14:44:57 -0400

    Use WP build in archive title in template.

--- wp-content/themes/my-website-theme/archive.php
+++ wp-content/themes/my-website-theme/archive.php
@@ -8,11 +8,7 @@ get_header(  );
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title">
       <?php
-        if (is_category(  )):
-          single_cat_title(  );
-        elseif (is_author(  )):
-          echo "Posts by "; the_author(  );
-        endif
+        the_archive_title(  );
       ?>
     </h1>
     <div class="page-banner__intro">
