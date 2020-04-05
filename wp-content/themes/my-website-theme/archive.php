84bda24e3882d1d7602f1bc2533087156b4a27f8
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-05 14:39:56 -0400

    Add dynamic title controls for archive template.

--- wp-content/themes/my-website-theme/archive.php
+++ wp-content/themes/my-website-theme/archive.php
@@ -6,7 +6,15 @@ get_header(  );
 <div class="page-banner">
   <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>);"></div>
   <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">A Static One</h1>
+    <h1 class="page-banner__title">
+      <?php
+        if (is_category(  )):
+          single_cat_title(  );
+        elseif (is_author(  )):
+          echo "Posts by "; the_author(  );
+        endif
+      ?>
+    </h1>
     <div class="page-banner__intro">
       <p>Learn how the school of your dreams got started.</p>
     </div>
