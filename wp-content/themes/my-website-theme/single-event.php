88d395cb62b72b9cbb036fdd76008dc133ed6969
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-04-07 11:11:19 -0400

    Add back link to Event:Archive

--- wp-content/themes/my-website-theme/single-event.php
+++ wp-content/themes/my-website-theme/single-event.php
@@ -22,8 +22,8 @@
   <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
       <p>
-        <a class="metabox__blog-home-link" href="<?php echo site_url( $path = '/blog') ?>">
-          <i class="fa fa-home" aria-hidden="true"></i> Blog Home
+        <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link( 'event' ); ?>">
+          <i class="fa fa-home" aria-hidden="true"></i> Event Archive
         </a>
         <span class="metabox__main">
           Posted by <?php the_author_posts_link(  ); ?> on <?php the_time( $d = 'd M, Y' ) ?> in <?php echo get_the_category_list( $separator = ', ' ) ?>
